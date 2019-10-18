80b33e367f52444fc5a9fdcbaf61c6ed1ca8206e
7xiezsla <tom123tomdog@gmail.com>
2019-10-18 23:25:24 +0800

    登入 登出 註冊 更新

--- domain/src/user.js
+++ domain/src/user.js
@@ -1,12 +1,15 @@
 const Coordinate = require('./coordinate')
-class User{
-  constructor(){
-    this.account = ''
-    this.password = ''
-    this.name = ''
-    this.score = 0
-    this.coordinate = new Coordinate()
-  }
+class User {
+    constructor() {
+        this.account = ''
+        this.password = ''
+        this.name = ''
+        this.sex = ''
+        this.age = ''
+        this.relation = []
+        this.score = 0
+        this.coordinate = new Coordinate()
+    }
 }
 
 module.exports = User
