96a5a373578871f730dc6c10ddc42b9342ed2f4c
7xiezsla <38800985+7xiezsla@users.noreply.github.com>
2019-10-19 23:38:46 +0800

    環境API 更新 排成修正

---
+++ backend/api/nasa/environment.js
@@ -0,0 +1,71 @@
+module.exports = (app) => {
+
+
+    app.get('/nasa/environment', async(req, res) => {
+
+        const getPipeline = (longitude, latitude) => {
+
+            const pipeline = [{
+                $addField: {
+                    deltaX: {
+                        $add: ['$longitude', -longitude]
+                    },
+                    deltaY: {
+                        $add: ['$latitude', -latitude]
+                    }
+                }
+            }, {
+                $addField: {
+                    distance: {
+                        $add: [
+                            { '$multiply': ['$deltaX', '$deltaX'] },
+                            { '$multiply': ['$deltaY', '$deltaY'] }
+                        ]
+                    }
+                }
+            }, {
+                $sort: {
+                    distance: -1
+                }
+            }, {
+                $limit: 1
+            }, {
+                $project: {
+                    _id: 0,
+                    distance: 0,
+                    longitude: 0,
+                    latitude: 0,
+                    deltaX: 0,
+                    deltaY: 0
+                }
+            }];
+
+        }
+
+        const client = await req.MongoClient.connect(req.mongoConnStr, req.mongoConnOpt);
+        const db = client.db('nasa');
+        const reqBody = req.query;
+        const range = 0.1;
+        const longitude = reqBody.longitude;
+        const latitude = reqBody.latitude;
+
+        const pipeline = getPipeline(longitude, latitude);
+
+        await db
+            .collection('weather')
+            .aggregate(pipeline)
+            .toArray()
+            .then(result1 => {
+                res.json(result1[0]);
+            })
+            .catch(e => {
+                res.sendStatus(500);
+            });
+
+        client.close();
+
+    });
+
+    return app;
+
+}
--- backend/routine/air.js
+++ backend/routine/air.js
@@ -1,12 +1,12 @@
 const fetch = require('node-fetch');
 const Mongo = require('mongodb');
 const MongoClient = Mongo.MongoClient;
-const mongoConnStr = 'mongodb://seal:seal1234@localhost:27017/admin';
+const mongoConnStr = 'mongodb://seal:seal1234@sealchang.asia:27017/admin';
 const mongoConnOpt = { useNewUrlParser: true, useUnifiedTopology: true };
 
 const main = async() => {
 
-    const getYYYYMMDDHH = () => {
+    const getYYYYMMDDHH = (i) => {
 
         const datetimeFix = (x) => {
 
@@ -21,7 +21,7 @@ const main = async() => {
         const y = String(date.getFullYear());
         const m = datetimeFix(date.getMonth() + 1);
         const d = datetimeFix(date.getDate());
-        const H = datetimeFix(date.getHours() - 1);
+        const H = datetimeFix(date.getHours() - i);
 
         yyyymmddhh = y + m + d + H;
 
@@ -29,49 +29,67 @@ const main = async() => {
 
     }
 
-    let data;
+    let data = null;
+
+    let cityIdArr = ['466940', '466900', '466880', '466930', '466910', '467571', '467490', '467770', '467350', '467650', '467530', '467550', '467480', '467410', '467440', '467590', '467080', '467060', '466990', '467610', '467660', '467540', '467620', '466950', '467300', '469020', '468100', '467420', 'A0W030', 'A0C540', 'D2F230', 'A0T780', 'A0A9M0', '466920', 'A0G720', 'A0Z080', 'A0W080', '467110', '467990', 'A0K420', '467050', 'A2K570', 'A0W100', 'CAAH60'];
+    let cityArr = ['基隆', '淡水', '土城', '陽明', '陽明', '新竹', '忠明', '沙鹿', '馬公', '埔里', '竹山', '埔里', '嘉義', '臺南', '小港', '恆春', '宜蘭', '冬山', '花蓮', '關山', '臺東', '潮州', '恆春', '基隆', '馬公', '金門', '金門', '安南', '金門', '土城', '三義', '花蓮', '新店', '萬華', '彰化', '埔里', '馬公', '金門', '馬祖', '麥寮', '觀音', '斗六', '金門', '古亭'];
 
     // await fetch('https://airtw.epa.gov.tw/json/AQI/Taiwan_' + '2019101910' + '.json')
-    await fetch('https://airtw.epa.gov.tw/json/AQI/Taiwan_' + getYYYYMMDDHH() + '.json')
-        .then(res => console.log(res.json()))
+    await fetch('https://airtw.epa.gov.tw/json/AQI/Taiwan_' + getYYYYMMDDHH(1) + '.json')
+        .then(res => res.json())
         .then(res => {
-            const newdata = res.cwbopendata.location.map(d => {
 
-                let onedata = {
-                    _id: d.stationId,
-                    longitude: d.lon,
-                    latitude: d.lat,
-                    locationName: d.locationName
+            let newdata = [];
+
+            res.forEach(d => {
+
+                const temp = d.txt.split(' ');
+
+                let idx = cityArr.indexOf(temp[0]);
+
+                while (idx !== -1) {
+
+                    const qry = { _id: cityIdArr[idx] };
+
+                    const udt = {
+                        $set: {
+                            GIS: temp[1].replace(/\(.*?\)/g, ''),
+                            GIS_Level: d.type
+                        }
+                    }
+
+                    newdata.push({ qry: qry, udt: udt });
+
+                    cityIdArr.splice(idx, 1);
+                    cityArr.splice(idx, 1);
+                    idx = cityArr.indexOf(temp[0]);
+
                 }
 
-                const schema = new Set(['WDSD', 'TEMP', 'HUMD', 'PRES', '24R', 'H_FX', 'H_UVI', 'D_TX', 'D_TN', 'H_Weather']);
-
-                d.weatherElement.forEach(k => {
-                    if (schema.has(k.elementName)) {
-                        onedata[k.elementName] = k.elementValue.value;
-                    }
-                })
-
-                return onedata;
             });
 
             data = newdata;
         })
         .catch(e => console.log(e));
 
-    // const client = await MongoClient.connect(req.mongoConnStr, req.mongoConnOpt);
+    console.log(data.length);
 
-    // const db = client.db('nasa');
-    // const weather = db.collection('weather');
+    if (data !== null) {
 
-    // data.forEach(d => {
-    //     await weather
-    //         .replaceOne(d)
-    //         .then(result1 => {})
-    //         .catch(e => {});
-    // })
+        const client = await MongoClient.connect(mongoConnStr, mongoConnOpt);
 
-    // client.close();
+        const db = client.db('nasa');
+        const weather = db.collection('weather');
+
+        data.forEach(async(d) => {
+            await weather
+                .updateOne(d.qry, d.udt)
+                .then(result1 => {})
+                .catch(e => { console.log(d, 'error') });
+        })
+
+        client.close();
+    }
 
 }
 
